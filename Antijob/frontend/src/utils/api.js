e9d62e51d4353e2a6794bce52b931c20ca327f7f
Meet2306 <22bmiit050@gmail.com>
2026-02-27 11:28:47 +0530

    URLChange

--- Antijob/frontend/src/utils/api.js
+++ Antijob/frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'job-portal-gamma-liard.vercel.app',
     withCredentials: true, // important for cookies
 });
 
