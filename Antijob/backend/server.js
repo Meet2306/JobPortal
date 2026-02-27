e43bfd737d86fb3e0a4acb018c6f2545971f5c28
Meet2306 <22bmiit050@gmail.com>
2026-02-27 11:41:53 +0530

    BACKENDUrlchnage

--- Antijob/backend/server.js
+++ Antijob/backend/server.js
@@ -15,7 +15,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // Vite default port
+    origin: [
+        "http://localhost:5173",
+        "https://job-portal-japb.vercel.app"
+    ],
     credentials: true
 }));
 
