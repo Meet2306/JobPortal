846391a3c4e345272dad2a594ad054a3a672faa2
Meet2306 <22bmiit050@gmail.com>
2026-02-27 12:05:28 +0530

    Cookies change

--- Antijob/backend/controllers/authController.js
+++ Antijob/backend/controllers/authController.js
@@ -76,8 +76,8 @@ exports.login = async (req, res) => {
         // Set HTTP-Only Cookie
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000
         });
 
