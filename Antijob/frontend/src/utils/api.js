f32c293bfc0f23e459aa0277faff016f4a4fe533
Meet2306 <22bmiit050@gmail.com>
2026-03-14 11:29:53 +0530

    URL

--- Antijob/frontend/src/utils/api.js
+++ Antijob/frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://job-portal-pi-two.vercel.app/api',
     withCredentials: true, // important for cookies
 });
 
