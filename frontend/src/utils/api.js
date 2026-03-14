a7253a05e03e07837456514fc808b3e3496048cf
Meet2306 <22bmiit050@gmail.com>
2026-03-14 15:32:37 +0530

    MistakeURL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://job-portal-xyzs-projects-9ffedc98.vercel.app/api',
+    baseURL: 'https://job-portal-xyzs-projects-9ffedc98.vercel.app',
     withCredentials: true, // important for cookies
 });
 
