5e29e5d24ef0207e37dc223409a71e49517ec068
Meet2306 <22bmiit050@gmail.com>
2026-03-20 11:18:31 +0530

    local

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://job-portal-backend-psi-flame.vercel.app/api',
+    baseURL: 'http://localhost:5000/api',
     withCredentials: true, // important for cookies
 });
 
