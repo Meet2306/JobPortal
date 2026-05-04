cd48c36f86285b84cf60340c7acb04dfeffae143
Meet2306 <22bmiit050@gmail.com>
2026-05-04 22:23:54 +0530

    BackendUrl

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://jobportal-7lf0.onrender.com/api',
     withCredentials: true, // important for cookies
 });
 
 export default api;
+// http://localhost:5000/api
