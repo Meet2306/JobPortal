f1cf861b526410c03f9b9c6fc2277b6c9c0c60a5
Meet2306 <22bmiit050@gmail.com>
2026-05-06 10:51:24 +0530

    newVercelApi

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://jobportal-psi-dun.vercel.app//api',
+    baseURL: 'https://jobportal-psi-dun.vercel.app/api',
     withCredentials: true, // important for cookies
 });
 
